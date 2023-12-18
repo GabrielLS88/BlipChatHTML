3477286525e99bf01841df5b78dce9f59d9cdc17
Gabriel Lopes <130192972+GabrielLS88@users.noreply.github.com>
2023-12-18 15:01:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@
       }
       buildChat = function() {
         builder = new BlipChat()
-          .withAppKey('dGVsZWtyb3V0ZXI6NDE1NGIzOTMtZDM0Yi00N2UxLThlZDAtZGFjZmNlMDNiNjg4')
-          .withCustomCommonUrl('https://telek.chat.blip.ai/')
+          .withAppKey('cm90ZWFkb3JiaW9mYXN0Ojg4MDgwNDBlLTQ4ZDYtNDc4OC05N2Q5LWRhOTI0ZmViYzIzMg==')
+          .withCustomCommonUrl('https://biofast.chat.blip.ai/'')
           .withButton({"color":"#2CC3D5","icon":""})
         builder.build()
-        builder.sendMessage("biofast")
+        builder.sendMessage("ola")
       }
       buildChat()
     }
